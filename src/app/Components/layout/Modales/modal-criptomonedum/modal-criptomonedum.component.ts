aee4c10bfc4e56c6ace8dee8cb9937d0dbb51ddf
AlexJara11 <alexanderjaramillo152@gmail.com>
2025-01-30 17:39:48 -0500

    Programación de componente y modal de CriptoMoneda

--- src/app/Components/layout/Modales/modal-criptomonedum/modal-criptomonedum.component.ts
+++ src/app/Components/layout/Modales/modal-criptomonedum/modal-criptomonedum.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { SharedModule } from '../../../../Reutilizable/shared/shared.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Criptomonedum } from '../../../../Interfaces/criptomonedum';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CriptomonedumService } from '../../../../Services/criptomonedum.service';
+import { UtilidadService } from '../../../../Reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-modal-criptomonedum',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './modal-criptomonedum.component.html',
   styleUrl: './modal-criptomonedum.component.css'
 })
-export class ModalCriptomonedumComponent {
-
+export class ModalCriptomonedumComponent implements OnInit {
+    formularioCriptomonedum:FormGroup;
+    tituloAccion:string = "Agregar";
+    botonAccion:string="Guardar";
+    listaCriptomonedum: Criptomonedum[] = [];
+    constructor(
+      private modalActual: MatDialogRef<ModalCriptomonedumComponent>,
+          @Inject(MAT_DIALOG_DATA) public datosCriptomonedum: Criptomonedum,
+          private fb: FormBuilder,
+          private _criptomonedumService: CriptomonedumService,
+          private _utilidadService: UtilidadService,
+    ){
+      this.formularioCriptomonedum = this.fb.group({
+        codigo: ['', Validators.required],
+        nombre: ['', Validators.required],
+        symbol: ['', Validators.required],
+        descripcionCriptomonedum: ['', Validators.required],
+        esActivo: ['1', Validators.required]
+      });
+      if(this.datosCriptomonedum != null){
+        this.tituloAccion = "Editar";
+        this.botonAccion = "Actualizar";
+      }
+    }
+    ngOnInit(): void {
+      if(this.datosCriptomonedum != null){
+        this.formularioCriptomonedum.patchValue({
+          idCriptomonedum: this.datosCriptomonedum.idCriptomonedum,
+          codigo: this.datosCriptomonedum.codigo,
+          nombre: this.datosCriptomonedum.nombre,
+          symbol: this.datosCriptomonedum.symbol,
+          descripcionCriptomonedum: this.datosCriptomonedum.descripcionCriptomonedum,
+          esActivo: this.datosCriptomonedum.esActivo.toString()
+        });
+      }
+    }
+    guardarEditar_Criptomonedum(){
+      const _criptomonedum: Criptomonedum ={
+        idCriptomonedum: this.datosCriptomonedum == null ? 0 : this.datosCriptomonedum.idCriptomonedum,
+        codigo: this.formularioCriptomonedum.value.codigo,
+        nombre: this.formularioCriptomonedum.value.nombre,
+        symbol : this.formularioCriptomonedum.value.symbol,
+        descripcionCriptomonedum: this.formularioCriptomonedum.value.descripcionCriptomonedum,
+        esActivo: parseInt(this.formularioCriptomonedum.value.esActivo)
+      }
+      if(this.datosCriptomonedum == null){
+        this._criptomonedumService.guardar(_criptomonedum).subscribe({
+          next: (data) => {
+            if(data.status){
+              this._utilidadService.mostrarAlerta("La Criptomoneda fue registrada", "Exito");
+              this.modalActual.close("true");
+            }else
+              this._utilidadService.mostrarAlerta("No se pudo registrar la Criptomoneda","Error");
+          },
+          error:(e) => {}
+        })
+      }else{
+        this._criptomonedumService.editar(_criptomonedum).subscribe({
+          next: (data) => {
+            if(data.status){
+              this._utilidadService.mostrarAlerta("La Criptomoneda fue editada", "Exito");
+              this.modalActual.close("true");
+            }else
+              this._utilidadService.mostrarAlerta("No se pudo editar la Criptomoneda","Error");
+          },
+          error:(e) => {}
+        })
+      }
+    }
 }
--- src/app/Components/layout/Pages/criptomonedum/criptomonedum.component.ts
+++ src/app/Components/layout/Pages/criptomonedum/criptomonedum.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { SharedModule } from '../../../../Reutilizable/shared/shared.module';
+import { Criptomonedum } from '../../../../Interfaces/criptomonedum';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { UtilidadService } from '../../../../Reutilizable/utilidad.service';
+import { CriptomonedumService } from '../../../../Services/criptomonedum.service';
+import { ModalCriptomonedumComponent } from '../../Modales/modal-criptomonedum/modal-criptomonedum.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-criptomonedum',
@@ -8,6 +16,79 @@ import { SharedModule } from '../../../../Reutilizable/shared/shared.module';
   templateUrl: './criptomonedum.component.html',
   styleUrl: './criptomonedum.component.css'
 })
-export class CriptomonedumComponent {
-
+export class CriptomonedumComponent implements OnInit, AfterViewInit {
+  columnasTabla: string[] = ['codigo', 'nombre', 'descripcion', 'estado', 'acciones'];
+  dataInicio: Criptomonedum[] = [];
+  dataListaCriptomonedum = new MatTableDataSource(this.dataInicio);
+  @ViewChild(MatPaginator) paginacionTabla!: MatPaginator;
+  constructor(
+    private dialog: MatDialog,
+    private _criptomonedumService: CriptomonedumService,
+    private _utilidadService: UtilidadService
+  ) { }
+  obtenerCriptomonedas() {
+    this._criptomonedumService.lista().subscribe({
+      next: (data) => {
+        if (data.status)
+          this.dataListaCriptomonedum.data = data.value;
+        else
+          this._utilidadService.mostrarAlerta("No se encontraron datos", "Oops!");
+      },
+      error: (e) => { }
+    })
+  }
+  ngOnInit(): void {
+    this.obtenerCriptomonedas();
+  }
+  ngAfterViewInit(): void {
+    this.dataListaCriptomonedum.paginator = this.paginacionTabla;
+  }
+  aplicarFiltroTabla(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataListaCriptomonedum.filter = filterValue.trim().toLocaleLowerCase();
+  }
+  nuevaCriptomoneda() {
+    this.dialog.open(ModalCriptomonedumComponent, {
+      disableClose: true
+    }).afterClosed().subscribe(resultado => {
+      if (resultado === "true") {
+        this.obtenerCriptomonedas();
+      }
+    });
+  }
+  editarProducto(criptomonedum: Criptomonedum) {
+    this.dialog.open(ModalCriptomonedumComponent, {
+      disableClose: true,
+      data: criptomonedum
+    }).afterClosed().subscribe(resultado => {
+      if (resultado === "true") {
+        this.obtenerCriptomonedas();
+      }
+    });
+  }
+  eliminarProducto(criptomonedum: Criptomonedum) {
+    Swal.fire({
+      title: '¿Desea eliminar la criptomoneda de la base de datos?',
+      text: criptomonedum.nombre,
+      icon: 'warning',
+      confirmButtonColor: '#3085d6',
+      confirmButtonText: "Sí, eliminar",
+      showCancelButton: true,
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'No, volver'
+    }).then((resultado) => {
+      if (resultado.isConfirmed) {
+        this._criptomonedumService.eliminar(criptomonedum.idCriptomonedum).subscribe({
+          next: (data) => {
+            if (data.status) {
+              this._utilidadService.mostrarAlerta("La criptomoneda fue eliminada de la base de datos", "Listo!");
+              this.obtenerCriptomonedas();
+            } else
+              this._utilidadService.mostrarAlerta("No se pudo eliminar la criptomoneda", "Error");
+          },
+          error: (e) => { }
+        })
+      }
+    });
+  }
 }
